65aad04505650002a9078e0aa5472ba709e6df14
xiatian0716 <819337877@qq.com>
2020-07-11 17:22:47 +0800

    04.02 axios及mock.js全局配置

--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -5,7 +5,13 @@
 </template>
 
 <script>
-export default {};
+export default {
+  mounted() {
+    this.$http.get("/home/getData").then(res => {
+      console.log(res.data);
+    });
+  }
+};
 </script>
 
 <style lang="scss" scoped></style>
