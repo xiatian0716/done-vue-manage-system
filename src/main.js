022f89dec92d1f2860e118c0b962b2d9301579fa
xiatian0716 <819337877@qq.com>
2020-07-12 23:58:18 +0800

    06 用户权限管理

--- src/main.js
+++ src/main.js
@@ -18,8 +18,27 @@ Vue.prototype.$http = http;
 
 Vue.config.productionTip = false;
 
+router.beforeEach((to, from, next) => {
+  // 防止刷新后vuex里丢失token
+  store.commit("getToken");
+  // 防止刷新后vuex里丢失标签列表tagList
+  store.commit("getMenu");
+  let token = store.state.user.token;
+  // 过滤登录页，防止死循环
+  if (!token && to.name !== "login") {
+    next({ name: "login" });
+  } else {
+    next();
+  }
+});
+
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: h => h(App),
+
+  // 刷新时触发操作
+  created() {
+    store.commit("addMenu", router);
+  }
 }).$mount("#app");
--- src/store/tab.js
+++ src/store/tab.js
@@ -1,3 +1,4 @@
+import Cookie from "js-cookie";
 export default {
   // state
   state: {
@@ -17,6 +18,56 @@ export default {
   },
   // 方法
   mutations: {
+    // 获取菜单-登录
+    setMenu(state, val) {
+      state.menu = val;
+      Cookie.set("menu", JSON.stringify(val));
+    },
+    // 清除菜单-登出
+    clearMenu(state) {
+      state.menu = [];
+      Cookie.remove("menu");
+    },
+    // 动态添加到路由
+    addMenu(state, router) {
+      // 从Cookie里面获取
+      if (!Cookie.get("menu")) {
+        return;
+      }
+      let menu = JSON.parse(Cookie.get("menu"));
+      // 将menu添加到state
+      state.menu = menu;
+      let currentMenu = [
+        {
+          path: "/",
+          component: () => import(`@/views/Main`),
+          // 动态添加的路由都放到children里
+          children: []
+        }
+      ];
+      // 循环操作来拼接路径
+      menu.forEach(item => {
+        if (item.children) {
+          item.children = item.children.map(item => {
+            item.component = () => import(`@/views/${item.url}`);
+            return item;
+          });
+          currentMenu[0].children.push(...item.children);
+        } else {
+          item.component = () => import(`@/views/${item.url}`);
+          currentMenu[0].children.push(item);
+        }
+      });
+      // 动态添加路由
+      router.addRoutes(currentMenu);
+    },
+    getMenu(state) {
+      if (Cookie.get("tagList")) {
+        let tagList = JSON.parse(Cookie.get("tagList"));
+        state.tabsList = tagList;
+      }
+    },
+
     // 点击后将val传进来
     selectMenu(state, val) {
       if (val.name !== "home") {
